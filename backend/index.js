e3584dae2ee169f628b833f165f0bf8c6a5873c3
Gopilal Anjaniya <gopilalanjaniya@gmail.com>
2026-07-14 23:31:46 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const ProductSchema = new mongoose.Schema({
     category: { type: String, required: true, trim: true },
 });
 const Product = mongoose.model("Product", ProductSchema);
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("Dashboard backend is running");
 });
 // ROUTES
